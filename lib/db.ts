fcd28a02c3692f32bc299f0bf6cd414c88998dec
Chyts15 <bjchyts@gmail.com>
2026-05-17 14:25:24 +0800

    simplify prisma client

--- lib/db.ts
+++ lib/db.ts
@@ -1,21 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
-function createPrismaClient() {
-    return new PrismaClient({
-        datasources: {
-            db: {
-                url: process.env.TURSO_DATABASE_URL
-                    ? process.env.TURSO_DATABASE_URL
-                    : process.env.DATABASE_URL!,
-            },
-        },
-    })
-}
-
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? createPrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
