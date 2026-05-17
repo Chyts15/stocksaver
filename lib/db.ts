73b171fec26c0696f1e2d0f6cfb413912e575bf7
Chyts15 <bjchyts@gmail.com>
2026-05-17 22:54:33 +0800

    switch to postgres neon

--- lib/db.ts
+++ lib/db.ts
@@ -4,27 +4,6 @@ const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined
 }
 
-function getClient() {
-    if (globalForPrisma.prisma) return globalForPrisma.prisma
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-    const url = process.env.DATABASE_URL ?? ''
-
-    if (!url || url.startsWith('file:')) {
-        const client = new PrismaClient()
-        globalForPrisma.prisma = client
-        return client
-    }
-
-    // Dynamic require so it's not evaluated at build time
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const { createClient } = require('@libsql/client')
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const { PrismaLibSql } = require('@prisma/adapter-libsql')
-    const libsql = createClient({ url })
-    const adapter = new PrismaLibSql(libsql)
-    const client = new PrismaClient({ adapter } as any)
-    globalForPrisma.prisma = client
-    return client
-}
-
-export const prisma = getClient()
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
